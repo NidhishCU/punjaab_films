95ccc7cf4f1f89d8f7903d2a14f52e3a73a78020
NidhishCU <NidhishCU@users.noreply.github.com>
2025-10-11 07:27:34 +0000

    deploy: 8ee27f8d6af09e35dcb75398a32df4dd5fde7ade

---
+++ sw.js
@@ -0,0 +1,24 @@
+// Minimal Service Worker - No fetch interception
+// This prevents the service worker from blocking resource loading
+
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installed');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated');
+  // Clean up old caches
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+// No fetch event listener - let all requests go through normally
